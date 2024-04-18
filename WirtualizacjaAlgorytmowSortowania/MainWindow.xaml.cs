16a47467805a5e99cf9d4ae42c4cb7c39c488fe6
Sebastian Wandzel <sebastianwandzel@yahoo.pl>
2024-04-19 00:11:54 +0200

    Menu 1.0 + Pobiweranie ciagu liczb

--- WirtualizacjaAlgorytmowSortowania/MainWindow.xaml.cs
+++ WirtualizacjaAlgorytmowSortowania/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WirtualizacjaAlgorytmowSortowania
 {
@@ -20,5 +12,63 @@ namespace WirtualizacjaAlgorytmowSortowania
         {
             InitializeComponent();
         }
+
+        private void btnSort_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                AddToList();
+            }
+            catch (FormatException FEx)
+            {
+                MessageBox.Show(FEx.Message);
+                return;
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+                return;
+            }
+
+
+            VisualizationWindow visualizationWindow = new VisualizationWindow();
+            this.Close();
+            visualizationWindow.Show();
+
+        }
+
+        private void EnterDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                btnSort_Click(sender, e);
+            }
+        }
+
+        public void AddToList()
+        {
+            string NumText = numbersString.Text;
+            NumText.Trim(',', ' ');
+            string[] numbersTab = NumText.Split(',', ' ');
+            if (numbersTab.Length < 2)
+            {
+                throw new Exception("No numbers to sort, it neet to be more than 1 number");
+            }
+
+            List<float> numbersList = new List<float>();
+            foreach (string num in numbersTab)
+            {
+                if (float.TryParse(num, out float number))
+                {
+                    numbersList.Add(number);
+                }
+                else
+                {
+                    throw new FormatException("Invalid number format");
+                }
+            }
+
+        }
+
     }
 }
---
+++ WirtualizacjaAlgorytmowSortowania/VisualizationWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+
+namespace WirtualizacjaAlgorytmowSortowania
+{
+    public partial class VisualizationWindow : Window
+    {
+
+        public VisualizationWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+
+        private void ReturnButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Close();
+            mainWindow.Show();
+        }
+    }
+}
